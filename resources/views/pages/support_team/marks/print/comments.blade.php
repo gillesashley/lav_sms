49f795913309f8620ee33ecde6fc5410303ee2cb
gillesashley <xylaray37@gmail.com>
2023-04-17 07:57:30 +0000

    changed GHC to ₵

--- resources/views/pages/support_team/marks/print/comments.blade.php
+++ resources/views/pages/support_team/marks/print/comments.blade.php
@@ -15,7 +15,7 @@
         </tr>
         <tr>
             <td><strong>NEXT TERM FEES:</strong></td>
-            <td><del>GHC</del>{{ $s['next_term_fees_'.strtolower($ct)] }}</td>
+            <td><del>₵</del>{{ $s['next_term_fees_'.strtolower($ct)] }}</td>
         </tr>
         </tbody>
     </table>
